ca35c4ee60b807ed880696a312cd4963b3ba58a9
Laziz <lazizo2004@gmail.com>
2023-07-04 17:28:56 +0500

    all tasks are done!

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,9 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class MethodParser {
 
     /**
@@ -20,6 +24,76 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        MethodSignature methodSignature;
+
+        String nonArgumentValues = signatureString.substring(0, signatureString.indexOf("("));
+        StringTokenizer st = new StringTokenizer(nonArgumentValues, " ");
+        int nonArgumentValuesCount = st.countTokens();
+
+        if(nonArgumentValuesCount == 2) {
+            String returnType = st.nextToken();
+            String methodName = st.nextToken();
+
+            String valuesInsideParenthesis = signatureString.substring(signatureString.indexOf("("));
+            StringTokenizer paramsInsideParenthesis = new StringTokenizer(valuesInsideParenthesis, " ,()");
+
+            List<MethodSignature.Argument> arguments = new ArrayList<>();
+
+            while (paramsInsideParenthesis.hasMoreTokens()) {
+                String type = paramsInsideParenthesis.nextToken();
+                String name = paramsInsideParenthesis.nextToken();
+                arguments.add(new MethodSignature.Argument(type, name));
+            }
+
+            methodSignature = new MethodSignature(methodName, arguments);
+            methodSignature.setReturnType(returnType);
+            return methodSignature;
+
+        } else {
+            String accessModifier = st.nextToken();
+            String returnType = st.nextToken();
+            String methodName = st.nextToken();
+
+            StringTokenizer insideParenthesis = new StringTokenizer(signatureString, "()");
+
+            if (insideParenthesis.countTokens() == 0) {
+                methodSignature = new MethodSignature(methodName);
+                methodSignature.setAccessModifier(accessModifier);
+                methodSignature.setReturnType(returnType);
+                return methodSignature;
+            } else {
+                String valuesInsideParenthesis = signatureString.substring(signatureString.indexOf("("));
+                StringTokenizer paramsInsideParenthesis = new StringTokenizer(valuesInsideParenthesis, " ,()");
+
+                List<MethodSignature.Argument> arguments = new ArrayList<>();
+
+                while (paramsInsideParenthesis.hasMoreTokens()) {
+                    String type = paramsInsideParenthesis.nextToken();
+                    String name = paramsInsideParenthesis.nextToken();
+                    arguments.add(new MethodSignature.Argument(type, name));
+                }
+
+                methodSignature = new MethodSignature(methodName, arguments);
+                methodSignature.setAccessModifier(accessModifier);
+                methodSignature.setReturnType(returnType);
+                return methodSignature;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String s = "String repeat(String value, int times)";
+        s = s.substring(s.indexOf("("));
+        StringTokenizer stringTokenizer1 = new StringTokenizer(s, "()");
+        System.out.println(stringTokenizer1.countTokens());
+
+
+
+        while(stringTokenizer1.hasMoreTokens())
+            System.out.println(stringTokenizer1.nextToken());
+
+
     }
 }
